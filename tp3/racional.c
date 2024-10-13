df2c3becd29622bbd766a3b27f56a29b1d1a1e24
Alejandro <alejandro.nava@ufpr.br>
2024-10-13 20:13:22 -0300

    Files

--- tp3/racional.c
+++ tp3/racional.c
@@ -8,8 +8,9 @@
  * funções auxiliares para facilitar a implementação daquelas funções.
 */
 
-/* coloque aqui seus includes (primeiro os <...>, depois os "...") */
 #include <stdio.h>
+#include <stdlib.h>
+#include "racional.h"
 
 /*
  * Implemente aqui as funcoes definidas no racionais.h; caso precise,
@@ -17,17 +18,6 @@
  * somente neste arquivo.
 */
 
-struct racional *cria_r(long num, long den){
-	
-	struct racional *ptr;
-	prt = malloc(sizeof(struct racional));
-
-	(*ptr).num = num;
-	(*prt).den = den;
-
-	return ptr;
-
-}
 
 int valido_r(struct racional *r){
 
@@ -48,8 +38,8 @@ long mmc (long a, long b){
 		a = b;
 		b = resto;
 	}
-	return labs(a) /* retorna o valor absoluto de a */
-}				   /* para evitar modificar o sinal dos racionais  */
+	return labs(a); /* retorna o valor absoluto de a */
+}				    /* para evitar modificar o sinal dos racionais  */
 
 /* Simplifica o número racional indicado no parâmetro.
  * Por exemplo, se o número for 10/8 muda para 5/4.
@@ -79,28 +69,65 @@ int simplifica_r (struct racional *r){
 	return 1;
 }
 
+struct racional *cria_r(long num, long den){
+	
+	struct racional *ptr;
+	ptr = malloc(sizeof(struct racional));
+
+	(*ptr).num = num;
+	(*ptr).den = den;
+	simplifica_r(ptr);
+	return ptr;
+
+}
+
+void destroi_r(struct racional *r){
+	
+	free(r);
+}
+
 void imprime_r(struct racional *r){
 
-	if(r == NULL){
+	if(r == NULL){	
 		printf("NULL");
-
-	} else if(!valido_r(r)){
-		printf("NaN");
-
-	} else if((*r).num == 0){
-		printf("0");
-
-	} else if((*r).den == 1){
-		printf("%ld", num);
-
-	} else if((*r).num == (*r).den){
-		printf("1");
-
-	} else {
-		printf("%ld/%ld", (*r).num, (*r).den);
+		return;
 	}
-		
+
+	if(!valido_r(r)){
+		printf("NaN");
+		return;
+	}
+
+	if((*r).num == 0){
+		printf("0");
+		return;
+	}
+
+	if((*r).den == 1){
+		printf("%ld", (*r).num);
+		return;
+	}
+
+	if((*r).num == (*r).den){
+		printf("1");
+		return;
+	}
+
+	printf("%ld/%ld", (*r).num, (*r).den);
+}
+
+int compara_r (struct racional *r1, struct racional *r2){
 	
+	if(r1 == NULL || r2 == NULL || !valido_r(r1) || !valido_r(r2))
+		return -2;
+
+	if((*r1).num * (*r2).den < (*r2).num * (*r1).den)
+		return -1;
+
+	if((*r2).num * (*r1).den < (*r1).num * (*r2).den)
+		return 1;
+
+	return 0;
 }
 
 /* Maximo Divisor Comum entre a e b      */
--- tp3/tp3.c
+++ tp3/tp3.c
@@ -25,14 +25,53 @@ void imprime_vetor(struct racional *v_ptr[], int n){
 	int i;
 
 	for(i = 0; i < n; i++){
-		imprime_r(*v_ptr);
+		imprime_r(v_ptr[i]);
+		printf(" ");
+	}
+	printf("\b\n");
+}
+
+void elimina_invalidos(struct racional *v_ptr[], int *n){
+	
+	int i;
+	i = 0;
+
+	while(i < *n){
+		if(!valido_r(v_ptr[i])){
+			destroi_r(v_ptr[i]);
+			v_ptr[i] = v_ptr[(*n)-1];
+			v_ptr[(*n)-1] = NULL;
+			(*n)--;
+		} else {
+			i++;
+		}
+	}
+}
+
+/* Ordena um vetor de racionais de maneira crescente */
+/* Utiliza o algoritmo de bubble sort  */
+void ordena_vetor(struct racional *v_ptr, int n){
+
+	int i, j;
+	for(i = n-1; i > 0; i--)
+		for(j = 0; j < i; j++)
+			if((compara_r(v_ptr[j], v_ptr[j+1])) == 1)
+				printf("bolsonaro");
+}
+
+void libera_memoria(struct racional **v_ptr, int n){
+	
+	int i;
+	for(i = 0; i < n; i++){
+		free(v_ptr[i]);
+		v_ptr[i] = NULL;
 	}
 }
 
 /* programa principal */
 int main (){
 
-	struct racional *v_ptr[];
+	struct racional **v_ptr;
 
 	int n;
 	scanf("%d", &n);
@@ -43,8 +82,15 @@ int main (){
 
 	printf("VETOR = ");
 	imprime_vetor(v_ptr, n);
-	
 
+	elimina_invalidos(v_ptr, &n);
+
+	printf("VETOR = ");
+	imprime_vetor(v_ptr, n);
+
+	libera_memoria(v_ptr, n);
+	free(v_ptr);
+	v_ptr = NULL;
 	return (0) ;
 }
 
